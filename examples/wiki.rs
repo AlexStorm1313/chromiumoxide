cad7d3e181c3d596dd469c33903c7faa79f9b499
AlexStorm13 <alexbrasser@gmail.com>
2024-04-10 10:04:16 +0000

    make errors go away

--- examples/wiki.rs
+++ examples/wiki.rs
@@ -1,32 +1,31 @@
-use futures::StreamExt;
-
 use chromiumoxide::browser::{Browser, BrowserConfig};
+use futures::StreamExt;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt::init();
+	tracing_subscriber::fmt::init();
 
-    let (browser, mut handler) = Browser::launch(BrowserConfig::builder().build()?).await?;
+	let (browser, mut handler) = Browser::launch(BrowserConfig::builder().build()?).await?;
 
-    let handle = async_std::task::spawn(async move {
-        loop {
-            let _ = handler.next().await.unwrap();
-        }
-    });
+	let handle = async_std::task::spawn(async move {
+		loop {
+			let _ = handler.next().await.unwrap();
+		}
+	});
 
-    let page = browser.new_page("https://en.wikipedia.org").await?;
+	let page = browser.new_page("https://en.wikipedia.org").await?;
 
-    page.find_element("input#searchInput")
-        .await?
-        .click()
-        .await?
-        .type_str("Rust (programming language)")
-        .await?
-        .press_key("Enter")
-        .await?;
+	page.find_element("input#searchInput")
+		.await?
+		.click()
+		.await?
+		.type_str("Rust (programming language)")
+		.await?
+		.press_key("Enter")
+		.await?;
 
-    let _html = page.wait_for_navigation().await?.content().await?;
+	let _html = page.wait_for_navigation().await?.get_content().await?;
 
-    handle.await;
-    Ok(())
+	handle.await;
+	Ok(())
 }
